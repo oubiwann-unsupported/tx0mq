6260f86516d4852e27fb2601a3e36323a9914487
Duncan McGreggor <duncan@dreamhost.com>
2011-12-22 19:04:49 -0800

    Added some thoughts about the ways in which a client API my be used.

--- examples/zguide/chapter01/client-server/hwclient.py
+++ examples/zguide/chapter01/client-server/hwclient.py
@@ -1,3 +1,29 @@
+"""
+How we might expect a tx0mq Twisted client API to look:
+
+    from twisted.python import log
+    from tx0mq.client import ZMQClient
+
+
+    def processResult(result):
+        print "Got something back! --> '%s'" % result
+
+    client = ZMQClient("tcp://127.0.0.1:5555")
+    deferred = client.send("Hello")
+    deferred.addCallback(processResult)
+    deferred.addErrback(log)
+
+We could even provide a custom reactor that adds ZeroMQ support. Usage could
+like like this:
+
+    from tx0mq import reactor
+    from tx0mq.client import ZMQClientFactory
+
+    factory = ZMQClientFactory()
+
+    reactor.connectZMQ("127.0.0.1", 5555, factory)
+    reactor.run()
+"""
 #
 #   Hello World client in Python
 #   Connects REQ socket to tcp://localhost:5555
